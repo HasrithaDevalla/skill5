be12462f79ba6f980eea56ff496b0f24c94c7645
HasrithaDevalla <devallahasritha@gmail.com>
2025-08-07 00:01:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ecommerce">
+    <BrowserRouter basename="/FRONTEND_ECOMMERCE">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
